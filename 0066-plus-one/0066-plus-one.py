fd042dbf91a3ee3d8a0ce7bcff2eb640feca3061
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2024-05-05 11:06:59 +0100

    Time: 36 ms (58.68%), Space: 16.5 MB (84.35%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = digits[::-1]
+        carry = 1
+        index = 0
+        while carry:
+            if index < len(digits):
+                if digits[index] == 9:
+                    digits[index] = 0
+                else:
+                    digits[index] += 1
+                    carry = 0
+
+            else:
+                digits.append(1)
+                carry = 0
+            index += 1
+        return digits[::-1]
