2530d5110d7308d7334d8acd4648352d77aa8a1b
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2024-05-10 11:20:33 +0100

    Time: 48 ms (39.52%), Space: 17 MB (91.75%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,8 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        result = ''
+        for i in range(len(s)):
+            if s[i] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz0123456789':
+                result += s[i].lower()
+        return result[::-1] == result
