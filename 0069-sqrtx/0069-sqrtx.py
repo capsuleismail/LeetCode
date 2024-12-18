43defcab5573c834a969dda125e75af074a75b8f
capsuleismail <81362009+capsuleismail@users.noreply.github.com>
2024-12-18 18:29:54 +0000

    Time: 3 ms (62.82%), Space: 18 MB (5.05%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,18 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x ** 0.5)
+        left, right = 1, x
+        
+        while left <= right:
+            middle = (left+right)//2
+            m_squared = middle*middle
+            
+            if m_squared == x:
+                return middle
+            elif m_squared < x:
+                left = middle + 1
+            else:
+                right = middle - 1
+        return right
+            
+                
+            
